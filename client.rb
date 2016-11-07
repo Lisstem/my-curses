e7063cb0b6bbab61ea8f456d496a134482048c6e
Thole <t.goesmann@tu-bs.de>
2016-11-07 14:30:17 +0100

    moved teststuff to test.rb changed addr

--- client.rb
+++ client.rb
@@ -17,54 +17,58 @@ class Client
 
 	def start!
 		@logger.info{'Connecting...'}
-		socket = TCPSocket.open('localhost', 28045)
-		@logger.info{"Connected to #{socket}"}
-
 		begin
-			puts('Enter nickname: ')
-			name = gets.chomp
-			puts("#{!("DISTRIBUTION=MINE;VERSION=PRE1;NAME=#{name}\n" =~ /^DISTRIBUTION=[^={}()\[\]:,;]*VERSION=[^={}()\[\]:,;]*;NAME=[^={}()\[\]:,;]*$/).nil?}")
-			socket.write("DISTRIBUTION=MINE;VERSION=PRE1;NAME=#{name}\n")
-		end while ((socket.gets =~ /^ACK.*$/).nil?)
-		@logger.info{"Successful registered as #{name}"}
+			socket = TCPSocket.open('lisstem.ddns.net', 28045)
+			@logger.info{"Connected to #{socket.addr}"}
+
+			begin
+				puts('Enter nickname: ')
+				name = gets.chomp
+				puts("#{!("DISTRIBUTION=MINE;VERSION=PRE1;NAME=#{name}\n" =~ /^DISTRIBUTION=[^={}()\[\]:,;]*VERSION=[^={}()\[\]:,;]*;NAME=[^={}()\[\]:,;]*$/).nil?}")
+				socket.write("DISTRIBUTION=MINE;VERSION=PRE1;NAME=#{name}\n")
+			end while ((socket.gets =~ /^ACK.*$/).nil?)
+			@logger.info{"Successful registered as #{name}"}
 
 
-		t = Thread.new{
+			t = Thread.new{
 				while (msg = socket.gets)
 					@logger.debug{"Received \"#{msg.chomp}\" from server."}
 				end
 			}
 
-		while (true)
-			text = gets.chomp.split(' ')
-			case cmd = text.shift
-			when 'q'
-				break
-			when 'exit'
-				msg = "EXIT\n"
-				socket.write(msg)
-				@logger.debug{"Send \"" + msg.chomp + "\" to server."}
-			when 'chat'
-				msg = "CHAT;#{text.join(' ')}\n"
-				socket.write(msg)
-				@logger.debug{"Send \"" + msg.chomp + "\" to server."}
-			when 'create'
-				msg = "CREATE;#{text[0]};#{text[1]}\n"
-				socket.write(msg)
-				@logger.debug{"Send \"" + msg.chomp + "\" to server."}
-			when 'lobbies'
-				msg = "LOBBIES\n"
-				socket.write(msg)
-				@logger.debug{"Send \"" + msg.chomp + "\" to server."}
-			when 'join'
-				msg = "JOIN;#{text[0]}\n"
-				socket.write(msg)
-				@logger.debug{"Send \"#{msg.chomp}\" to server."}
-			else
-				msg = "#{cmd.upcase};#{text.join(';')}\n"
-				socket.write(msg)
-				@logger.debug{"Send \"#{msg.chomp}\" to server."}
+			while (true)
+				text = gets.chomp.split(' ')
+				case cmd = text.shift
+					when 'q'
+						break
+					when 'exit'
+						msg = "EXIT\n"
+						socket.write(msg)
+						@logger.debug{"Send \"" + msg.chomp + "\" to server."}
+					when 'chat'
+						msg = "CHAT;#{text.join(' ')}\n"
+						socket.write(msg)
+						@logger.debug{"Send \"" + msg.chomp + "\" to server."}
+					when 'create'
+						msg = "CREATE;#{text[0]};#{text[1]}\n"
+						socket.write(msg)
+						@logger.debug{"Send \"" + msg.chomp + "\" to server."}
+					when 'lobbies'
+						msg = "LOBBIES\n"
+						socket.write(msg)
+						@logger.debug{"Send \"" + msg.chomp + "\" to server."}
+					when 'join'
+						msg = "JOIN;#{text[0]}\n"
+						socket.write(msg)
+						@logger.debug{"Send \"#{msg.chomp}\" to server."}
+					else
+						msg = "#{cmd.upcase};#{text.join(';')}\n"
+						socket.write(msg)
+						@logger.debug{"Send \"#{msg.chomp}\" to server."}
+				end
 			end
+		rescue => ex
+			@logger.fatal{"\n#{ex.backtrace.join("\n")}: #{ex.message} (#{ex.class})"}
 		end
 		t.kill
 		socket.close
